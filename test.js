4f27e9340373253a630e9e19f1c490ef3e84ce06
parkerDesktop <smitty6123@gmail.com>
2022-08-24 15:02:31 -0400

    adding toLowerCase() to function singleRound

--- test.js
+++ test.js
@@ -50,72 +50,88 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+console.log(getComputerChoice(), "//bot pick");
 
-let pick = prompt("Enter your 3 choices: Rock, Paper, Scissors");
 
-const playerSelection = "rock" "paper"; //getComputerChoice() two bots playing against each other
+// let pick = prompt("Enter your 3 choices: Rock, Paper, Scissors");
+
+const playerSelection = "rock"; //getComputerChoice() two bots playing against each other
 const computerSelection = getComputerChoice();
 
 function singleRound(playerSelection, computerSelection) {
-    // if (pick?.toLowerCase(playerSelection > computerSelection) ) {
-    //     return "You win!";
-    // } else if (pick?.toLowerCase(playerSelection < computerSelection) ) {
-    //     return "You lose :(";
-    // } else {
-    //     return "it's a tie";
-    // }
-
-    // console.log(playerSelection)
-    // console.log(computerSelection)
-
-
-    if (playerSelection == "rock" && computerSelection == "scissors" || 
-        playerSelection == "paper" && computerSelection == "rock" ||
-        playerSelection == "scissors" && computerSelection == "paper") {
+    if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
         return "You win!";
-    } else if (playerSelection == computerSelection)  {
+    } else if (playerSelection.toLowerCase() == computerSelection) {
         return "it's a tie";
     } else {
         return "You lose :(";
     }
-    // return null;
 }
+
+console.log(playerSelection, "//always rock for now");
+
+// function sinRound(playerSelection, computerSelection) {
+//     if (pick?.toLowerCase(playerSelection > computerSelection) ) {
+//             return "You win!";
+//         } else if (pick?.toLowerCase(playerSelection < computerSelection) ) {
+//                 return "You lose :(";
+//             } else {
+//                     return "it's a tie";
+//                 }
+                
+                // console.log(computerSelection)
+                
+                
+                // if (playerSelection == "rock" && computerSelection == "scissors" || 
+                // playerSelection == "paper" && computerSelection == "rock" ||
+                // playerSelection == "scissors" && computerSelection == "paper") {
+                //     return "You win!";
+                // } else if (playerSelection == computerSelection)  {
+                //     return "it's a tie";
+                // } else {
+                //     return "You lose :(";
+                // }
+    // return null;
 
 console.log(singleRound(playerSelection, computerSelection) );
 
+// comment out everything below
+/*
+
 // console.log(prompt());
 
-//this works
-/* let pick = prompt("Enter your 3 choices: Rock, Paper, Scissors");
+// this works
+//  let pick = prompt("Enter your 3 choices: Rock, Paper, Scissors");
 
-if (pick.toLowerCase() === "rock") {
-    alert("you chose Rock");
-} */
-
-//ignored call prompt(), iframe sandbox "allow-modals" keyword not set
-// function allowModals(){
-//     for (const i of document.getElementsByTagName('iframe')) {
-//       if (!i.sandbox.supports('allow-modals')) {
-//         console.warn("Your browser doesn't support the 'allow-modals' attribute :(");
-//         break;
-//       }
-//       if (i.sandbox.contains('allow-modals')) continue;
-//       console.info(i, "doesn't allow modals");
-//       i.sandbox.add('allow-modals');
-//       console.info(i, 'now allows modals');
-//     }
-//   }
-
-function game() {
-    function singleRound(){
-            for (let i = 0; i < 5; i++){
-                console.log(i);
-            }
-        }
+// if (pick.toLowerCase() === "rock") {
+    //     alert("you chose Rock");
+    // } 
+    
+    //ignored call prompt(), iframe sandbox "allow-modals" keyword not set
+    // function allowModals(){
+        //     for (const i of document.getElementsByTagName('iframe')) {
+            //       if (!i.sandbox.supports('allow-modals')) {
+                //         console.warn("Your browser doesn't support the 'allow-modals' attribute :(");
+                //         break;
+                //       }
+                //       if (i.sandbox.contains('allow-modals')) continue;
+                //       console.info(i, "doesn't allow modals");
+                //       i.sandbox.add('allow-modals');
+                //       console.info(i, 'now allows modals');
+                //     }
+                //   }
+                
+                function game() {
+                    function singleRound(){
+                        for (let i = 0; i < 5; i++){
+                            console.log(i);
+                        }
+                    }
     }
 
 // console.log(game());
 
 // let messagePrompt = "enter your text here";
 // prompt(messagePrompt);
+
+*/
